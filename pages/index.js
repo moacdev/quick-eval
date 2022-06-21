e153117392d4eaa03fbea54c85fdc50af558870f
moacdev <moacdev@gmail.com>
2022-06-21 01:18:53 +0000

    v1 simple

--- pages/index.js
+++ pages/index.js
@@ -3,95 +3,177 @@ import Image from 'next/image'
 import { Input, Select, Option, Accordion, AccordionHeader, AccordionBody } from "@material-tailwind/react";
 import { useState } from 'react';
 
+class Eval {
+  constructor(type, name){
+    this.type = type,
+    this.name = name,
+    this.cuisine = {
+      presentation_service : 0,
+      presentation_plat : 0,
+      gout : 0
+    }
+    this.danse = {
+      originalite : 0,
+      occupation_scenique : 0,
+      expression_du_corps : 0
+    }
+    this.organisation_group = {
+      presentation_du_stand : 0,
+      orginalite_de_la_decoration : 0,
+      coordination_des_activites : 0
+    }
+  }
+}
 
 export default function Home() {
   
   const [activeEval, setActiveEval] = useState(-1)
   const [data, setData] = useState([
-      {
-        type: 'ethnie',
-        name: 'Bambara',
-        evals: {
-          presentation_service: '',
-          presentation_plat: '',
-          gout: '',
+    new Eval('ethnie', 'Bambara'),
+    new Eval('ethnie', 'FORGERON'),
+    new Eval('ethnie', 'Les FILS DU DESERT'),
+    new Eval('ethnie', 'MALINKE/KASSONKE'),
+    new Eval('ethnie', 'MIANKA'),
+    new Eval('ethnie', 'BOZO'),
+    new Eval('ethnie', 'DOGON'),
+    new Eval('pays', 'CONGO'),
+    new Eval('pays', 'GABON'),
+    new Eval('pays', 'COTE D\'IVOIRE'),
+    new Eval('pays', 'BURKINA FASSO'),
+    new Eval('pays', 'AFRIQUE DU SUD'),
+    new Eval('pays', 'CAMEROUN'),
+  ]);
+
+  const [selectedEval, setSelectedEval] = useState(-1)
+  const [selectedActivity, setSelectedActivity] = useState(-1)
+  const [selectedCritere, setSelectedCritere] = useState(-1)
+
+  const activites = [
+    {
+      name: 'cuisine',
+      label: 'PRESENTATION CUISINE',
+      criteres: [
+        {
+          name: "presentation_service",
+          label: "Présentation du service",
         },
-      },
-      {
-        type: 'ethnie',
-        name: 'Soninké',
-        evals: {
-          presentation_service: '',
-          presentation_plat: '',
-          gout: '',
+        {
+          name: "presentation_plat",
+          label: "Présentation du plat",
         },
-      },
-      {
-        type: 'country',
-        name: 'Congo',
-        evals: {
-          presentation_service: '',
-          presentation_plat: '',
-          gout: '',
+        {
+          name: "gout",
+          label: "Le Goût",
         },
-      },
-      {
-        type: 'country',
-        name: 'Algerie',
-        evals: {
-          presentation_service: '',
-          presentation_plat: '',
-          gout: '',
+      ]
+    },
+    {
+      name: 'danse',
+      label: 'DANSE TRADITIONNELLE',
+      criteres: [
+        {
+          name: "originalite",
+          label: "Originalité",
         },
-      },
-    ]);
- 
+        {
+          name: "occupation_scenique",
+          label: "Occupation scénique",
+        },
+        {
+          name: "expression_du_corps",
+          label: "Expression du corps",
+        },
+      ]
+    },
+    {
+      name: 'organisation_group',
+      label: 'ORGANISATION DU GROUPE',
+      criteres: [
+        {
+          name: "presentation_du_stand",
+          label: "Présentation du stand",
+        },
+        {
+          name: "orginalite_de_la_decoration",
+          label: "Orginalité de la décoration",
+        },
+        {
+          name: "coordination_des_activites",
+          label: "Coordination des activités",
+        },
+      ]
+    },
+    
+    
+  ]
+
+  const getTotal = (_criteres) => {
+    let sum = 0.0;
+    for (const _ in _criteres) sum += _criteres[_] == '' ? 0 : parseInt(_criteres[_])
+    return parseFloat(sum / Object.keys(_criteres).length).toFixed(2) ;
+  } 
+
+  const getTotalPts = (..._criteres) => {
+    let superTotal = 0.0;
+    for (let i = 0; i < _criteres.length; i++) {
+      superTotal = parseFloat(superTotal + parseFloat(getTotal(_criteres[i])))
+      console.log(_criteres[i]);
+      
+    }
+    
+    return superTotal;
+  }
+
   return (
-    <div className='flex flex-col max-w-2xl w-full mx-auto min-h-screen items-center justify-center'>
+    <div className='flex flex-col gap-4 max-w-2xl w-full mx-auto min-h-screen items-center justify-center'>
       <h1>Notation communautaire</h1>
-      {activeEval == -1 && (<div className='flex flex-col max-w-2xl w-full mx-auto min-h-screen items-center justify-center'>
-        <h2>Ethnies</h2>
-        <div className='flex flex-wrap'>
-          {data.map( (e,i) => e.type == 'ethnie' ? <button key={i} onClick={() => setActiveEval(i)} type="button" className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">{e.name}</button> : '' )}
-        </div>
-        <h2>Pays</h2>
-        <div className='flex flex-wrap'>
-        {data.map( (e,i) => e.type == 'country' ? <button key={i} onClick={() => setActiveEval(i)} type="button" className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">{e.name}</button> : '' )}
-        </div>
+      <div className="flex items-end gap-4 w-full">
+        <Select variant="outlined" label="Ethnies/Pays" >
+          {data.map( (e, i) => (<Option key={i} onClick={ ()=> setSelectedEval(i) }>{e.name}</Option>) )}
+        </Select>
+        <Select variant="standard" label="Activité">
+          {activites.map( (a, i) => <Option key={i} onClick={ ()=> setSelectedActivity(i) }>{a.label}</Option> )}
+        </Select>
+        {selectedActivity != -1 && (<Select variant="static" label="critère">
+          {activites[selectedActivity].criteres.map( (c, i) => (<Option key={i} onClick={ ()=> setSelectedCritere(i)} >{c.label}</Option>) )}
+        </Select>)}
+      </div>
+      { selectedEval != -1 && selectedActivity != -1 && selectedCritere != -1  &&(
+      <div className='w-full'>
+        <div className='h-16 w-16 mx-auto'>
+        <Input label="Note" className='h-16' value={ data[selectedEval][activites[selectedActivity].name][activites[selectedActivity].criteres[selectedCritere].name]} onChange={ (_) => { setData( [...data].map( (e,i) => {
+          if (i == selectedEval) {
+            e[activites[selectedActivity].name][activites[selectedActivity].criteres[selectedCritere].name] = _.currentTarget.value
+            return e;
+          }
+          else return e;
+          } ) ); console.log(data);  } } /></div>
+          <div className='flex flex-col gap-2 w-full max-w-sm mt-6 mx-auto rounded p-4'>
+            <h1 className='text-grey-900'>Récapitilatif</h1>
+            <p className='text-grey-900'>Ethnie/pays: <span className='bg-grey-900 text-white rounded p-2'>{data[selectedEval].name}</span></p>
+            {activites.map( (a, i) => 
+            <>
+              <p className='text-grey-900'>
+                {a.label} :
+              </p>
+              <div className='bg-grey-900 ml-6 p-2'>
+                {activites[selectedActivity].criteres.map( (c, _i) => 
+                <p className='text-white'>
+                  {c.label} : {data[selectedEval][activites[i].name][activites[i].criteres[_i].name]}
+                </p>
+                )}
+                <span className='text-blue-600'>Total: { getTotal(data[selectedEval][activites[i].name]) } </span>
+              </div>
+            </>
+            )}
+            <p>
+            Total de point : <span className='bg-grey-900 text-white rounded p-2'>
+              {getTotalPts(data[selectedEval][activites[0].name], data[selectedEval][activites[1].name], data[selectedEval][activites[2].name])}
+              </span>
+            </p>
+          </div>
       </div>)}
-      {activeEval != -1 &&(<EvalScreen data={data} setActiveEval={ _ => setActiveEval(_)} index={activeEval} handleChanges={ (_, __) => {let ___ = data; ___[__] = _; setData(___) } } />)}
+      
     </div>
   )
-}
-
-function EvalScreen({data,index, handleChanges, setActiveEval}) {
-  const [open, setOpen] = useState(0);
-  return <div className='flex flex-col items-end gap-4 w-full relative'>
-    <button onClick={ () => setActiveEval(-1)}>Retour</button>
-    <h2></h2>
-      <Accordion open={open === 1} onClick={() => setOpen(1)}>
-        <AccordionHeader>PRESENTATION CUISINE</AccordionHeader>
-        <AccordionBody className='w-full max-w-xs mx-auto flex flex-col gap-2'>
-          <Input label="Présentation du service" type="number" value={data[index].evals.presentation_service} onChange={ _ => handleChanges(_.currentTarget.value, index) } />
-          <Input label="Présentation du plat" type="number" value={data[index].evals.presentation_plat} onChange={ _ => handleChanges(_.currentTarget.value, index) }  />
-          <Input label="Le Goût" type="number" value={data[index].evals.gout} onChange={ (_) => { handleChanges(_.currentTarget.value, index)} }  />
-        </AccordionBody>
-      </Accordion>
-      <Accordion open={open === 2} onClick={() => setOpen(2)}>
-        <AccordionHeader>DANSE TRADITIONNELLE</AccordionHeader>
-        <AccordionBody className='w-full max-w-xs mx-auto flex flex-col gap-2'>
-          <Input label="Présentation du service" type="number" />
-          <Input label="Présentation du plat" type="number" />
-          <Input label="Le Goût" type="number" />
-        </AccordionBody>
-      </Accordion>
-      <Accordion open={open === 3} onClick={() => setOpen(3)}>
-        <AccordionHeader>ORGANISATION DU GROUPE</AccordionHeader>
-        <AccordionBody className='w-full max-w-xs mx-auto flex flex-col gap-2'>
-          <Input label="Présentation du service" type="number" />
-          <Input label="Présentation du plat" type="number" />
-          <Input label="Le Goût" type="number" />
-        </AccordionBody>
-      </Accordion>
-  </div>
 }
